3516d7de597ebb6936bc0ea141f92e7201702982
DLinh0811 <linhnda11@gmail.com>
2024-08-10 14:29:25 +0930

    commit

--- block1/PartySeats/PartySeats.java
+++ block1/PartySeats/PartySeats.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.Collections;
 public class PartySeats {
     public static void main(String[] args) {
-        String[] attendees = {"BOB boy","SUZIE girl","DAVE boy","JO girl","AL boy","BOB boy","CARLA girl","DEBBIE girl"};
+        String[] attendees = {"BOB boy","SUZIE girl","Ch girl", "Fu boy"};
         String[] result = seating(attendees);
         System.out.println(Arrays.toString(result));
     }
@@ -26,14 +26,14 @@ public class PartySeats {
         Collections.sort(Boys);
         Collections.sort(Girls);
 
-        if (Boys.size() == Girls.size() && (attendees.length + 2) % 2 == 0) {
+        if (Boys.size() == Girls.size() && attendees.length % 4 == 0) {
             
             String[] seat_order = new String[attendees.length + 2];
             seat_order[0] = "HOST";
-            seat_order[(seat_order.length/2)+1] = "HOSTESS";
-            System.out.println(seat_order);
+            seat_order[(attendees.length/2+1)] = "HOSTESS";
+            
             for (int i = 1; i < seat_order.length; i+=2) {
-                if (i == ((seat_order.length/2)+1)) {
+                if (i == ((attendees.length/2+1))) {
                     continue;
                 }else{
                     if (index_girls < Girls.size()) {
@@ -42,16 +42,17 @@ public class PartySeats {
                     }
                 }
             }
+            
             for (int i = 2; i < seat_order.length; i+=2) {
-                if (i == ((seat_order.length/2)+1)) {
-                    continue;
-                }else{
+                
+                
                     if (index_boys < Boys.size()) {
                         seat_order[i] = Boys.get(index_boys);
                         index_boys ++;
                     }
                 }
-            }
+                
+           
             return seat_order;
         }
         else{
